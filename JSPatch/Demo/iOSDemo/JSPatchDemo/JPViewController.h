8e9ab03169e8da11dbee7f066129f8800aa60f34
ye.ye <ye.ye@immomo.com>
2019-02-19 15:41:04 +0800

    添加jspatch文件

---
+++ JSPatch/Demo/iOSDemo/JSPatchDemo/JPViewController.h
@@ -0,0 +1,15 @@
+//
+//  JPViewController.h
+//  JSPatch
+//
+//  Created by bang on 15/5/2.
+//  Copyright (c) 2015年 bang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPViewController : UIViewController
+
+- (void)_hello;
+
+@end
