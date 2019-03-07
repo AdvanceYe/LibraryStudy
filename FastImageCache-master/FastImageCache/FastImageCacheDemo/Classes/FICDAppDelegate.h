854b35647873867bf2bfd4f1f04625f2f3356d3b
tataball <>
2019-03-07 09:11:21 +0800

    commit

---
+++ FastImageCache-master/FastImageCache/FastImageCacheDemo/Classes/FICDAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  FICDAppDelegate.h
+//  FastImageCacheDemo
+//
+//  Copyright (c) 2013 Path, Inc.
+//  See LICENSE for full license agreement.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FICDViewController;
+
+@interface FICDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) FICDViewController *viewController;
+
+@end
---
+++ FastImageCache-master/FastImageCache/FastImageCacheDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastImageCacheDemo
+//
+//  Created by Rui Peres on 17/06/2015.
+//  Copyright (c) 2015 Path. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FICDAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FICDAppDelegate class]));
+    }
+}
